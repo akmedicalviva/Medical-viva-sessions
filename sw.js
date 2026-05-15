177426dd438f2c4715f58629d4d7f0811726c2c6
akmedicalviva <akjshopqr@gmail.com>
2026-05-15 16:46:29 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'medical-viva-v3.0';
+const urlsToCache = [
+    '/',
+    '/index.html'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+            .catch(err => console.log('Cache failed:', err))
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+    return self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+            .catch(() => caches.match('/index.html'))
+    );
+});
